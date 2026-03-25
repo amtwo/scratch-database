00c9a6dfde317b8ca1cd7258c705a899b79557b5
Andy Mallon (ᴀᴍ²) <amtwo@users.noreply.github.com>
2026-03-25 07:52:10 -0400

    Rename DeletedAt column to DroppedAt in SQL table

--- tables/dbo.ObjectExpiration.sql
+++ tables/dbo.ObjectExpiration.sql
@@ -8,7 +8,7 @@ CREATE TABLE dbo.ObjectExpiration
 	CreatedBy	nvarchar(128),
 	KeepUntil   datetime2(0)
 		CONSTRAINT DF_ObjectExpiration_KeepUntil DEFAULT (DATEADD(DAY,90,SYSUTCDATETIME())),
-	DeletedAt   datetime2(0),
+	DroppedAt   datetime2(0),
 	CONSTRAINT PK_Keeplist PRIMARY KEY CLUSTERED (ObjectId) WITH (DATA_COMPRESSION = PAGE),
 	INDEX IX_ObjectExpiration_KeepUntil NONCLUSTERED (KeepUntil) WHERE DeletedAt IS NULL WITH (DATA_COMPRESSION = PAGE),
     INDEX IX_ObjectExpiration_DeletedAt NONCLUSTERED (DeletedAt) WITH (DATA_COMPRESSION = PAGE)
