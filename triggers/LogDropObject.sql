644c9fe47f713ed04c279aee6088b9a9cedc60e1
Andy Mallon (ᴀᴍ²) <amtwo@users.noreply.github.com>
2026-03-25 08:01:08 -0400

    Refactor LogDropObject trigger to remove ObjectType
    Removed unused variable declaration for ObjectType and updated the column name in the update statement.

--- triggers/LogDropObject.sql
+++ triggers/LogDropObject.sql
@@ -7,16 +7,14 @@ BEGIN
 
     DECLARE @EventData XML = EVENTDATA();
     DECLARE @ObjectName sysname = @EventData.value('(/EVENT_INSTANCE/ObjectName)[1]', 'sysname');
-    DECLARE @ObjectType sysname = @EventData.value('(/EVENT_INSTANCE/ObjectType)[1]', 'sysname');
+    --DECLARE @ObjectType char(2) = @EventData.value('(/EVENT_INSTANCE/ObjectType)[1]', 'sysname');
 
-    PRINT 'Object dropped: ' + @ObjectName + ' of type ' + @ObjectType;
-    PRINT OBJECT_ID(@ObjectName)
-
+    
     /* we just dropped the object, so the objectid is gone. Need to use the name. */
     UPDATE oe
-    SET DeletedAt = SYSUTCDATETIME()
+    SET DroppedAt = SYSUTCDATETIME()
     FROM dbo.ObjectExpiration AS oe
     WHERE oe.ObjectName = @ObjectName
-    AND DeletedAt IS NULL;
+    AND DroppedAt IS NULL;
 END;
 GO
